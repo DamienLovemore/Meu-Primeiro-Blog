1488e5ffdc695d3251d902223c696f1b15fbf91e
DamienLovemore <matheusoaresmartins2020@gmail.com>
2021-01-13 11:20:02 -0300

    Meu Primeiro Blog, quarto commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post #Import a classe Post do arquivo models que está dentro desta pasta(.)
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/post_list.html',{'posts':posts})
