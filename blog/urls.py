f75d86d71a264cb12166aa6e09ada2bc4857eef1
DamienLovemore <matheusoaresmartins2020@gmail.com>
2021-01-13 15:21:08 -0300

    Meu Primeiro Blog, quinto commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views #Importa todas as views que encontrar nessa pasta.
 
 urlpatterns = [
-    path('',views.post_list,name='post_list')
+    path('',views.post_list,name='post_list'),
+    path('post/<int:pk>/',views.post_detail,name='post_detail')
 ]
